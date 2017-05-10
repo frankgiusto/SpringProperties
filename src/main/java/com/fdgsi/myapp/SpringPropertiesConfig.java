d85c6ec7e6ae797c93d2f020a52ef5163c71bb63
frankgiusto <fgiust1240@rogers.com>
2017-05-10 16:38:27 -0400

    Update SpringPropertiesConfig.java

--- src/main/java/com/fdgsi/myapp/SpringPropertiesConfig.java
+++ src/main/java/com/fdgsi/myapp/SpringPropertiesConfig.java
@@ -1,80 +1,19 @@
-package com.fdgsi.myapp;
-
+package ca.gc.ic.cipo.tm.madridconsole.web;
 import java.util.Properties;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import javax.sql.DataSource;
-
-import org.apache.commons.configuration.DatabaseConfiguration;
-import org.apache.commons.dbcp.BasicDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertiesPropertySource;
-import org.springmodules.commons.configuration.CommonsConfigurationFactoryBean;
-
-/**
- * Example of Spring 4 Properties Java Configuration,
- *  with a Database Properties table to store most values
- *  and a small application.properties file too.
- * The Database table will take precedence over the properties file with this setup
- */
-@Configuration
-@PropertySource(value = { "classpath:application.properties" }, ignoreResourceNotFound=true)
-public class SpringPropertiesConfig {
-    private static final Logger log = LoggerFactory.getLogger(SpringPropertiesConfig.class);
-
-    @Inject
-    private org.springframework.core.env.Environment env;
-
-    @PostConstruct
-    public void initializeDatabasePropertySourceUsage() {
-        MutablePropertySources propertySources = ((ConfigurableEnvironment) env).getPropertySources();
-
-        try {
-            //dataSource, Table Name, Key Column, Value Column
-            DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration(dataSource(), "AppConfiguration", "propertyKey", "propertyValue");
-            
-            //CommonsConfigurationFactoryBean comes from https://java.net/projects/springmodules/sources/svn/content/tags/release-0_8/projects/commons/src/java/org/springmodules/commons/configuration/CommonsConfigurationFactoryBean.java?rev=2110
-            //Per https://jira.spring.io/browse/SPR-10213 I chose to just copy the raw source into our project
-            CommonsConfigurationFactoryBean commonsConfigurationFactoryBean = new CommonsConfigurationFactoryBean(databaseConfiguration);
-            
-            Properties dbProps = (Properties) commonsConfigurationFactoryBean.getObject();
-            PropertiesPropertySource dbPropertySource = new PropertiesPropertySource("dbPropertySource", dbProps);
-
-            //By being First, Database Properties take precedence over all other properties that have the same key name
-            //You could put this last, or just in front of the application.properties if you wanted to...
-            propertySources.addFirst(dbPropertySource);
-        } catch (Exception e) {
-            log.error("Error during database properties setup", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Read why this is required: http://www.baeldung.com/2012/02/06/properties-with-spring/#java
-     * It is important to be static: http://www.java-allandsundry.com/2013/07/spring-bean-and-propertyplaceholderconf.html
-     */
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
-    
-    @Bean
-    public DataSource dataSource()
-    {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(env.getProperty("jdbc.url"));
-        dataSource.setUsername(env.getProperty("jdbc.username"));
-        dataSource.setPassword(env.getProperty("jdbc.password"));
-        return dataSource;
-    }
-    
-}
+import org.apache.commons.configuration.DatabaseConfiguration;import org.slf4j.Logger;import org.slf4j.LoggerFactory;import org.springframework.beans.factory.InitializingBean;import org.springframework.context.EnvironmentAware;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;import org.springframework.context.annotation.PropertySource;import org.springframework.context.annotation.PropertySources;import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;import org.springframework.core.env.ConfigurableEnvironment;import org.springframework.core.env.Environment;import org.springframework.core.env.MutablePropertySources;import org.springframework.core.env.PropertiesPropertySource;import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;import org.springmodules.commons.configuration.CommonsConfigurationFactoryBean;
+/** * Spring 4 Properties Java Configuration, with a Database Properties table to store most values and a small * application.properties file too. The Database table will take precedence over the properties file with this setup */@Configuration@PropertySources({    @PropertySource(value = "classpath:application-configuration.properties", ignoreResourceNotFound = false),    @PropertySource("classpath:configuration.properties")})
+public class SpringPropertiesConfig extends PropertySourcesPlaceholderConfigurer    implements EnvironmentAware, InitializingBean {
+    private static final Logger log = LoggerFactory.getLogger(SpringPropertiesConfig.class);
+    private Environment environment;
+    @Override    public void setEnvironment(Environment environment) {        // save off Environment for later use        this.environment = environment;        super.setEnvironment(environment);    }
+    @Override    public void afterPropertiesSet() throws Exception {
+        MutablePropertySources envPropSources = ((ConfigurableEnvironment) environment).getPropertySources();
+        try {
+            // dataSource, Table Name, Key Column, Value Column            DatabaseConfiguration databaseConfiguration = new DatabaseConfiguration(mydataSource(),                environment.getProperty("config.table.name"), environment.getProperty("config.table.key"),                environment.getProperty("config.table.value"));
+            CommonsConfigurationFactoryBean commonsConfigurationFactoryBean = new CommonsConfigurationFactoryBean(                databaseConfiguration);
+            Properties dbProps = (Properties) commonsConfigurationFactoryBean.getObject();            PropertiesPropertySource dbPropertySource = new PropertiesPropertySource("dbPropertySource", dbProps);
+            // By being First, Database Properties take precedence over all other properties that have the same key name            // You could put this last, or just in front of the application.properties if you wanted to...            envPropSources.addFirst(dbPropertySource);        } catch (Exception e) {            log.error("Error during database properties setup", e);            throw new RuntimeException(e);        }
+    }
+    @Bean    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {        return new PropertySourcesPlaceholderConfigurer();    }
+    @Bean    public DataSource mydataSource() {        final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();        dsLookup.setResourceRef(true);        DataSource dataSource = dsLookup.getDataSource(environment.getProperty("config.jndiName"));        return dataSource;    }}
